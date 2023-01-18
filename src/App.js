ab3fdd96ee8698c491376ce2b6515e751a2c4178
NicoleBorbe <nicole.borbe@gmail.com>
2023-01-18 21:12:42 +0100

    Test 2

--- src/App.js
+++ src/App.js
@@ -12,29 +12,12 @@ function App() {
         </p>
 <div>Hello Jenni was here</div>
           Learn React
-        </a>
+        
       </header>
     </div>
   );
 
-    return (
-        <div className='App'>
-            <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-
-                <div>Hallo Welt</div>
-
-
-                <h1>Saddam</h1>
-
-                <div>Hallo Welt, von Nicole</div>
-
-            </header>
-        </div>
-    );
+    
 
 }
 
